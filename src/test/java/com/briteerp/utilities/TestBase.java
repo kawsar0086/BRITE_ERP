2036cca6e32939f5c35c243e3960027a83e0bd1b
“Zulyar <kurbanzulyar@gmail.com>
2019-01-30 23:26:15 -0500

    reset all utilities classes

--- src/test/java/com/briteerp/utilities/TestBase.java
+++ src/test/java/com/briteerp/utilities/TestBase.java
@@ -1,90 +1,81 @@
 package com.briteerp.utilities;
 
-import com.aventstack.extentreports.ExtentReporter;
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+import com.aventstack.extentreports.reporter.configuration.Theme;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.ITestResult;
+import org.testng.annotations.*;
+
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
-import org.openqa.selenium.WebDriver;
-import org.testng.ITestResult;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeTest;
 
 public abstract class TestBase {
     protected WebDriver driver;
     protected Pages pages;
+
     protected static ExtentReports report;
     private static ExtentHtmlReporter htmlReporter;
     protected static ExtentTest extentLogger;
 
-    public TestBase() {
-    }
 
-    @BeforeMethod(
-            alwaysRun = true
-    )
+    @BeforeMethod(alwaysRun = true)
     public void setupMethod() {
-        this.driver = Driver.getDriver();
-        this.pages = new Pages();
-        this.driver.manage().timeouts().implicitlyWait(10L, TimeUnit.SECONDS);
-        this.driver.get(ConfigurationReader.getProperty("url"));
+        driver = Driver.getDriver();
+        pages = new Pages();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get(ConfigurationReader.getProperty("url"));
     }
 
-    @AfterMethod(
-            alwaysRun = true
-    )
+    @AfterMethod(alwaysRun = true)
     public void tearDownMethod(ITestResult result) throws IOException {
-        if (result.getStatus() == 2) {
+        // if any test fails, it can detect it,
+        // take a screen shot at the point and attach to report
+        if (result.getStatus() == ITestResult.FAILURE) {
             String screenshotLocation = BrowserUtils.getScreenshot(result.getName());
             extentLogger.fail(result.getName());
             extentLogger.addScreenCaptureFromPath(screenshotLocation);
             extentLogger.fail(result.getThrowable());
-        } else if (result.getStatus() == 3) {
+
+        } else if (result.getStatus() == ITestResult.SKIP) {
             extentLogger.skip("Test Case Skipped: " + result.getName());
         }
-
         Driver.closeDriver();
     }
 
-    @BeforeTest(
-            alwaysRun = true
-    )
+    @BeforeTest(alwaysRun = true)
     public void setUpTest() {
         report = new ExtentReports();
+        // this is our custom location of the report that will be generated
+        // report will be generated in the current project inside folder: test-output
+        // report file name: report.html
         String filePath = System.getProperty("user.dir") + "/test-output/report.html";
+
+//        windows users pls correct ur path:
+//        String filePath = System.getProperty("user.dir") + "\\test-output\\report.html";
+
+        // initialize the htmlReporter with the path to the report
         htmlReporter = new ExtentHtmlReporter(filePath);
-        report.attachReporter(new ExtentReporter[]{htmlReporter});
+
+        // we attach the htmlreport to our report
+        report.attachReporter(htmlReporter);
+
         report.setSystemInfo("Environment", "Staging");
         report.setSystemInfo("Browser", ConfigurationReader.getProperty("browser"));
         report.setSystemInfo("OS", System.getProperty("os.name"));
+
         report.setSystemInfo("QA Engineer", "Admiral Kunkka");
+
         htmlReporter.config().setDocumentTitle("Prestashop Reports");
         htmlReporter.config().setReportName("Prestashop Automated Test Reports");
+
+//        htmlReporter.config().setTheme(Theme.DARK);
+
     }
-
-<<<<<<< HEAD
-
-
-    @AfterMethod
-    public void afterEachTest() {
-        try {
-            Thread.sleep(5000);
-        } catch(Exception e) {
-            e.getStackTrace();
-        }
-        if (driver != null) {
-            driver.quit();
-            driver = null;
-        }
-=======
-    @AfterTest(
-            alwaysRun = true
-    )
+    @AfterTest(alwaysRun = true)
     public void tearDownTest() {
         report.flush();
->>>>>>> 7961694dea84fc931147a80686dee821d40f54d9
     }
 }
