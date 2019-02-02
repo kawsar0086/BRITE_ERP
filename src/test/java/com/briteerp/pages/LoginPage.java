b128a0670f15bba96bece38f62b48926de578272
ramnaf <45299045+ramnaf@users.noreply.github.com>
2019-02-02 18:53:38 -0500

    Update LoginPage.java
    resmiiii zeriktimmmmmm

--- src/test/java/com/briteerp/pages/LoginPage.java
+++ src/test/java/com/briteerp/pages/LoginPage.java
@@ -9,7 +9,7 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
-
+//first push...
 public class LoginPage extends TestBase {
     public  LoginPage(){
         PageFactory.initElements(Driver.getDriver(),this);
